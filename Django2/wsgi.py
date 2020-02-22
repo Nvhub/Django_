4715d9fc8e455f2797b8ebedc26dbd70955b84c8
Your Name <you@example.com>
2020-02-22 02:07:47 -0500

    added all files

--- Django2/wsgi.py
+++ Django2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for local project.
+WSGI config for Django2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "local.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Django2.settings")
 
 application = get_wsgi_application()
---
+++ locallibrery/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LocallibreryConfig(AppConfig):
+    name = 'locallibrery'
